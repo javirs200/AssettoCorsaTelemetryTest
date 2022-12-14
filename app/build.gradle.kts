ebeaafe4a67a2ba00ed9d6143ca5274a584a3837
rsmax <fordrsmax@gmail.com>
2022-12-15 00:42:49 +0100

    fix again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("AssettoCorsaTelemetryTest.App")
+    mainClass.set("App")
 }
 
 tasks.named<Test>("test") {
